a9310bf60075e4d4d7e1060a39672063f60f430c
THLea <brokaya@163.com>
2022-04-25 21:39:12 +0800

    给蓝牙增加了发送数据的功能

--- app/src/main/java/com/example/uavhostcomputer/activity/MainActivity.java
+++ app/src/main/java/com/example/uavhostcomputer/activity/MainActivity.java
@@ -13,6 +13,7 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.content.BroadcastReceiver;
@@ -21,6 +22,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -36,8 +38,12 @@ import com.example.uavhostcomputer.R;
 import com.example.uavhostcomputer.tool_class.ActivityManagement;
 import com.example.uavhostcomputer.tool_class.BlueToothTool;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collector;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
@@ -51,6 +57,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     private static final String ACTION_GATT_CONNECTED = "com.example.uavhostcomputer.ACTION_GATT_CONNECTED";
     private static final String ACTION_GATT_DISCONNECTING = "com.example.uavhostcomputer.ACTION_GATT_DISCONNECTING";
     private static final String ACTION_GATT_DISCONNECTED = "com.example.uavhostcomputer.ACTION_GATT_DISCONNECTED";
+    private static final String ACTION_GATT_SEND_SUCCESSFUL = "com.example.uavhostcomputer.ACTION_GATT_SEND_SUCCESSFUL";
+    private static final String ACTION_GATT_SEND_FAIL = "com.example.uavhostcomputer.ACTION_GATT_SEND_FAIL";
     //参数分成多少份
     private static int PARAM_STEP_MAX = 100;
     //param_map的key，用来区分不同的参数
@@ -70,9 +78,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
         @Override
         public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-            _update_param(R.id.p_param_bar);
-            _update_param(R.id.i_param_bar);
-            _update_param(R.id.d_param_bar);
+            if(charSequence.length() != 0) {
+                _update_param(R.id.p_param_bar);
+                _update_param(R.id.i_param_bar);
+                _update_param(R.id.d_param_bar);
+            }
         }
 
         @Override
@@ -101,6 +111,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private boolean permission_is_opened = false;
     private boolean is_connect = false;
+
+    private UUID write_UUID_service;
+    private UUID write_UUID_chara;
+    private UUID read_UUID_service;
+    private UUID read_UUID_chara;
+    private UUID notify_UUID_service;
+    private UUID notify_UUID_chara;
+    private UUID indicate_UUID_service;
+    private UUID indicate_UUID_chara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -133,6 +152,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                         if(is_connect && BlueToothTool.getGatt() != null){
                             BlueToothTool.getGatt().disconnect();
                             BlueToothTool.setGatt(null);
+                            is_connect = false;
                         }
                         goto_choose_ble();
                     } else {
@@ -168,6 +188,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         } else {
             _normal_exception("相关权限未开启，不能够使用蓝牙功能");
         }
+
     }
 
     private void goto_choose_ble(){
@@ -194,23 +215,105 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 return false;
             }
         }
-        BlueToothTool.setGatt(selected_device.connectGatt(this, false, gattCallback));
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+            BlueToothTool.setGatt(selected_device.connectGatt(this, false, gattCallback, BluetoothDevice.TRANSPORT_LE));
+        else
+            BlueToothTool.setGatt(selected_device.connectGatt(this, false, gattCallback));
         connect_status.setText("连接中");
 
         return true;
     }
 
+    @SuppressLint("MissingPermission")
     private void send_data(){
-
+        BluetoothGattService service = BlueToothTool.getGatt().getService(write_UUID_service);
+        BluetoothGattCharacteristic characteristic = service.getCharacteristic(write_UUID_chara);
+        byte[] data = param_map.toString().getBytes();
+        if(data.length > 20){
+            Log.d(TAG, "send_data: data_length = "+data.length);
+            int num = 0;
+            num = data.length/20;
+            if(data.length % 20 != 0){
+                num = num + 1;
+            }
+            for(int i = 0; i < num; i++){
+                byte[] tempArr;
+                if(i == num-1){
+                    tempArr = new byte[data.length - i*20];
+                    System.arraycopy(data, i*20, tempArr, 0, data.length- i*20);
+                } else {
+                    tempArr = new byte[20];
+                    System.arraycopy(data, i*20, tempArr, 0, 20);
+                }
+                Log.d(TAG, "tempArr["+i+"]send_data: "+ Arrays.toString(tempArr));
+                characteristic.setValue(tempArr);
+                BlueToothTool.getGatt().writeCharacteristic(characteristic);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        } else {
+            characteristic.setValue(data);
+            BlueToothTool.getGatt().writeCharacteristic(characteristic);
+        }
     }
 
+    @SuppressLint("MissingPermission")
+    private void receive_data(){
+        BluetoothGattCharacteristic characteristic = BlueToothTool.getGatt().
+                getService(read_UUID_service).getCharacteristic(read_UUID_chara);
+        BlueToothTool.getGatt().readCharacteristic(characteristic);
+    }
+
+    public List<BluetoothGattService> getSupportedGattServices(){
+        if(BlueToothTool.getGatt() == null)
+            return null;
+        return BlueToothTool.getGatt().getServices();
+    }
+
+    private void initServiceAndChara(){
+        List<BluetoothGattService> services = getSupportedGattServices();
+        for(BluetoothGattService service : services){
+            List<BluetoothGattCharacteristic> charas = service.getCharacteristics();
+            for(BluetoothGattCharacteristic chara : charas){
+                int charaProp = chara.getProperties();
+                if((charaProp & BluetoothGattCharacteristic.PROPERTY_READ) > 0){
+                    read_UUID_service = service.getUuid();
+                    read_UUID_chara = chara.getUuid();
+                    Log.d(TAG, "read_service:"+read_UUID_service+" read_characteristic:"+read_UUID_chara);
+                }
+                if((charaProp & BluetoothGattCharacteristic.PROPERTY_WRITE) > 0){
+                    write_UUID_service = service.getUuid();
+                    write_UUID_chara = chara.getUuid();
+                    Log.d(TAG, "write_service:"+write_UUID_service+" write_characteristic:"+write_UUID_chara);
+                }
+                if((charaProp & BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0){
+                    notify_UUID_service = service.getUuid();
+                    notify_UUID_chara = chara.getUuid();
+                    Log.d(TAG, "notify_service:"+notify_UUID_service+" notify_characteristic:"+notify_UUID_chara);
+                }
+                if((charaProp & BluetoothGattCharacteristic.PROPERTY_INDICATE) > 0){
+                    indicate_UUID_service = service.getUuid();
+                    indicate_UUID_chara = chara.getUuid();
+                    Log.d(TAG, "indicate_service:"+indicate_UUID_service+" indicate_characteristic:"+indicate_UUID_chara);
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
     private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+
+        //连接状态改变时调用这个函数
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             super.onConnectionStateChange(gatt, status, newState);
             switch (newState){
                 case BluetoothProfile.STATE_CONNECTED:{
                     sendBroadcast(new Intent(ACTION_GATT_CONNECTED));
+                    BlueToothTool.getGatt().discoverServices();
                     break;
                 }
                 case BluetoothProfile.STATE_CONNECTING:{
@@ -223,31 +326,59 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                 }
                 case BluetoothProfile.STATE_DISCONNECTED:{
                     sendBroadcast(new Intent(ACTION_GATT_DISCONNECTED));
+                    BlueToothTool.setGatt(null);
                     break;
                 }
             }
         }
 
+        //寻找到服务时调用这个函数
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             super.onServicesDiscovered(gatt, status);
+            if(status == BluetoothGatt.GATT_SUCCESS){
+                //初始化服务和特征值
+                initServiceAndChara();
+                //订阅通知
+                BlueToothTool.getGatt().setCharacteristicNotification(
+                        BlueToothTool.getGatt().getService(notify_UUID_service).getCharacteristic(notify_UUID_chara), true);
+            }
+
         }
 
+        //读取设备时调用这个函数
         @Override
         public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
             super.onCharacteristicRead(gatt, characteristic, status);
+            Toast.makeText(MainActivity.this,"读取数据", Toast.LENGTH_SHORT).show();
         }
 
+        //写设备时调用这个函数
+        @Override
+        public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+            super.onCharacteristicWrite(gatt, characteristic, status);
+            Log.d(TAG, "onCharacteristicWrite: status = "+status+" value="+ Arrays.toString(characteristic.getValue()));
+            if(status == BluetoothGatt.GATT_SUCCESS){
+                sendBroadcast(new Intent(ACTION_GATT_SEND_SUCCESSFUL));
+            } else {
+                sendBroadcast(new Intent(ACTION_GATT_SEND_FAIL));
+            }
+        }
+
+        //设备发出通知时调用这个
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
             super.onCharacteristicChanged(gatt, characteristic);
+
         }
 
+        //向设备Descriptor写入时调用这个
         @Override
         public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
             super.onDescriptorWrite(gatt, descriptor, status);
         }
 
+        //读取到rssi时调用这个
         @Override
         public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
             super.onReadRemoteRssi(gatt, rssi, status);
@@ -417,16 +548,24 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
                         connect_status.setText("连接中");
                         break;
                     }
-                    case ACTION_GATT_DISCONNECTED:{
+                    case ACTION_GATT_DISCONNECTING:{
                         Log.i(TAG, "正在断开设备");
                         is_connect = false;
                         connect_status.setText("正在断开设备");
                         break;
                     }
-                    case ACTION_GATT_DISCONNECTING:{
+                    case ACTION_GATT_DISCONNECTED:{
                         Log.i(TAG, "已断开连接");
                         is_connect = false;
                         connect_status.setText("已断开");
+                        break;
+                    }
+                    case ACTION_GATT_SEND_SUCCESSFUL:{
+                        Toast.makeText(MainActivity.this, "发送成功", Toast.LENGTH_SHORT).show();
+                        break;
+                    }
+                    case ACTION_GATT_SEND_FAIL:{
+                        Toast.makeText(MainActivity.this, "发送失败", Toast.LENGTH_SHORT).show();
                         break;
                     }
                 }
@@ -440,6 +579,8 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         intentFilter.addAction(ACTION_GATT_CONNECTING);
         intentFilter.addAction(ACTION_GATT_DISCONNECTING);
         intentFilter.addAction(ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(ACTION_GATT_SEND_SUCCESSFUL);
+        intentFilter.addAction(ACTION_GATT_SEND_FAIL);
 
         registerReceiver(gattUpdateReceiver, intentFilter);
     }
